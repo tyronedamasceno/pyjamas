b1015a8bfb794f533cef30685a25e5cd0e9e35b1
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-12-13 23:33:23 -0300

    Add BFS

--- graph1.py
+++ graph1.py
@@ -3,18 +3,35 @@ Exemplo 1
 
 Implementação de um grafo simples
 """
-
 from collections import defaultdict
+from queue import Queue
+
 
 def find_with_dfs(graph, st, tgt):
     visited = set()
-    def _dfs(cur, tgt):
+    def _dfs(cur):
         visited.add(cur)
         for neighbor, _ in graph.graph[cur]:
             if neighbor not in visited:
-                _dfs(neighbor, tgt)
+                _dfs(neighbor)
     _dfs(st, tgt)
     return tgt in visited
+
+
+def find_with_bfs(graph, st, tgt):
+    visited = set()
+    q = Queue()
+    q.put(st)
+
+    while not q.empty():
+        cur = q.get()
+        visited.add(cur)
+        for neighbor, _ in graph.graph[cur]:
+            if neighbor not in visited:
+                q.put(neighbor)
+
+    return tgt in visited
+
 
 class Graph:
     def __init__(self):
@@ -27,3 +44,10 @@ class Graph:
     def show_graph(self):
         for node, neighbors in self.graph.items():
             print(f'{node}:  {neighbors}')        
+
+my_graph = Graph()
+my_graph.add_edge(1, 2)
+my_graph.add_edge(1, 3)
+my_graph.add_edge(2, 4)
+my_graph.add_edge(2, 5)
+my_graph.add_edge(3, 6)
